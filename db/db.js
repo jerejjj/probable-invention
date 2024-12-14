d36251b9069c06f0fc049b71b310f51a1b839645
jerejjj <131805062+jerejjj@users.noreply.github.com>
2024-12-14 18:21:16 +0200

    Add files via upload

--- db/db.js
+++ db/db.js
@@ -1,11 +1,12 @@
 import { Client } from "https://deno.land/x/postgres/mod.ts";
 
 // Set up PostgreSQL client connection
+
 const client = new Client({
   user: "postgres",
   database: "postgres",
   hostname: "localhost",
-  password: "Secret1234!",
+  password: "ECRET1234",
   port: 5432,
 });
 
